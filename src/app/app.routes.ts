ed433a215f451ce60617c58ec32e880291129804
TomasOliver <tomascampanelli@hotmail.com>
2025-11-17 23:01:37 -0300

    Mostrar Lista Equipos y Jugadores

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { TeamsSelectionComponent } from './equipos/teams-selection-component/teams-selection-component';
+import { PlantelComponent } from './equipos/plantel-component/plantel-component'; 
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'listaTeams', component: TeamsSelectionComponent},
+    {path: 'listaTeams/:id', component: PlantelComponent},
+    {path: '**', component: TeamsSelectionComponent}
+];
